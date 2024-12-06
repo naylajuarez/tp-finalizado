ad967157abda1a7887f23b005b81065bef18765e
naylajuarez <juareznayla072@gmail.com>
2024-12-06 20:21:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+setTimeout(function () {
+    alert('Bienvenido a mi pagina!');
+}, 1000);
+
+function cambiarfondo() {
+    document.body.style.backgroundColor= "black";
+    document.getElementById("main").style.backgroundColor = "black";
+    document.getElementById("header").style.backgroundColor = "rgba(75, 75, 73, 0.847);";
+    document.body.header.style.backgroundColor = "#90ffe9";
+    document.getElementById("nav").style.backgroundColor = "whitesmoke";
+    document.getElementById("nav").style.color = "black";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("section").style.color = "whitesmoke";
+    document.getElementById("aside").style.backgroundColor = "black";
+    document.getElementById("footer").style.backgroundColor = "black";;
+}
+
+
